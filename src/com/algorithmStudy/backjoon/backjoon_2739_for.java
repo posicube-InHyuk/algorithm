5ff8f630a3611036c62dd5374bc816f6402af4a4
Dplo1514 <dladlsgur3334@gmail.com>
2022-06-14 15:05:22 +0900

    backjoon_2739_for

---
+++ src/com/algorithmStudy/backjoon/backjoon_2739_for.java
@@ -0,0 +1,17 @@
+package com.algorithmStudy.backjoon;
+
+import java.util.Scanner;
+
+public class backjoon_2739_for {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        int i = 1 ;
+
+        while (i < 10) {
+            System.out.println(input + " * " + i + " = " + input * i);
+            i++;
+        }
+    }
+}
