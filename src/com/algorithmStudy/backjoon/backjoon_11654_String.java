ba4807ab81b41143546da8f26204de2bcbee3a11
Dplo1514 <dladlsgur3334@gmail.com>
2022-06-18 19:57:50 +0900

    backjoon_11654_String

--- src/com/algorithmStudy/backjoon/backjoon_11654_String.java
+++ src/com/algorithmStudy/backjoon/backjoon_11654_String.java
@@ -1,5 +1,16 @@
 package com.algorithmStudy.backjoon;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class backjoon_11654_String {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        byte[] bytes = input.getBytes(StandardCharsets.UTF_8);
+
+        System.out.println(bytes[0]);
+    }
 }
