3841ed5a265037130d0ea9f51c98cccd87896d16
Dplo1514 <dladlsgur3334@gmail.com>
2022-06-30 23:45:51 +0900

    backjoon_10872_factorial

---
+++ src/com/algorithmStudy/backjoon/backjoon_10872_factorial.java
@@ -0,0 +1,24 @@
+package com.algorithmStudy.backjoon;
+
+import java.util.Scanner;
+
+public class backjoon_10872_factorial {
+
+    static int factorialRecursion(int param) {
+        if (param == 1) {
+            return 1;
+        }
+
+        return param * factorialRecursion(param - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int param = scanner.nextInt();
+
+        int solution1 = factorialRecursion(param);
+
+        System.out.println("solution1 = " + solution1);
+
+    }
+}
