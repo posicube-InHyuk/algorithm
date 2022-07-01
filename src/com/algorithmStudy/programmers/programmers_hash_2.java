c3475b13f076ee804dfbb9f152beeda4e4dd9e98
Dplo1514 <dladlsgur3334@gmail.com>
2022-07-01 23:47:25 +0900

    programmers_hash_전화번호 목록

---
+++ src/com/algorithmStudy/programmers/programmers_hash_2.java
@@ -0,0 +1,22 @@
+package com.algorithmStudy.programmers;
+
+public class programmers_hash_2 {
+    static class Solution {
+
+        public boolean solution(String[] phoneBook) {
+            boolean answer = true;
+            
+
+
+
+            return answer;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String [] phoneBook = {"119", "97674223", "1195524421"};
+        Solution solution = new Solution();
+        solution.solution(phoneBook);
+    }
+}
