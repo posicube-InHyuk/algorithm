96884779e111113c814439871b2e3d8777ecf09d
Dplo1514 <dladlsgur3334@gmail.com>
2023-03-02 09:52:51 +0900

    SelectionSort_알고리즘 개념 주석 추가

--- src/com/algorithmStudy/basic/Sort/SelectionSort.java
+++ src/com/algorithmStudy/basic/Sort/SelectionSort.java
@@ -1,6 +1,16 @@
 package com.algorithmStudy.basic.Sort;
 
 public class SelectionSort {
+    /*
+    or문을 통해 가장 작은 값을 찾고, 맨 앞자리와 교환
+    다음 for문에선 맨 앞자리값을 제외한 값 중 가장 작은 값을 찾고, 두번째 앞자리와 교환
+    이 작업을 최대 n - 1 번 반복하면 정렬 완료 -> 시간복잡도 : O(n^2)
+    ex) 3 4 1 5 2 를 정렬한다고 가정
+    1번째 loop => 최소값 = 1 -> 1 4 3 5 2 => 1 확정
+    2번째 loop => 최소값 = 2 -> 1 2 3 5 4 => 2 확정
+    3번째 loop => 최소값 = 3 -> 1 2 3 5 4 => 3 확정
+    4번째 loop => 최소값 = 4 -> 1 2 3 4 5 => 4확정, 5확정
+    */
 
     static void selectionSortDesc(int[] arr) {
         int desc = 0;
