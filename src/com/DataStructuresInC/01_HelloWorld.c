3a1cd9ad1cfd2ad626a58a6e4f048e320703559b
Dplo1514 <dladlsgur3334@gmail.com>
2023-06-08 22:53:20 +0900

    1. C언어로 배우는 자료구조 시작 HelloWorld

---
+++ src/com/DataStructuresInC/01_HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>            //전처리기 : 필요한 헤더파일 정의
+
+int main(void)                //함수 헤더
+{                             //함수 시작 블록
+    printf("Hello World\n");  //화면 출력
+
+    return 0;                 //함수가 종료되면서 값을 반환
+}                             //함수 종료 블록
