404abc0cd0506efc861a62b658231262fcd6fe6c
Dplo1514 <dladlsgur3334@gmail.com>
2023-07-06 18:24:11 +0900

    1. Programmers 문제 풀이

--- src/com/algorithmStudy/Test/Company/Company_01.java
+++ src/com/algorithmStudy/Test/Company/Company_01.java
@@ -1,11 +1,7 @@
 package com.algorithmStudy.Test.Company;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Company_01 {
-    public static int[] reverseArr(int[] arr){
+    public static int[] Q1ReverseArr(int[] arr){
         int len = arr.length;
         int[] result = new int[len];
 
@@ -16,9 +12,25 @@ public class Company_01 {
         return result;
     }
 
+    public static int[] Q2TargetArr(int[] arr){
+        int len = arr.length;
+        int target = 0;
+        int[] result = new int[len];
+
+        for (int i = 0; i < arr.length; i++) {
+            target = arr[i];
+
+            if (target != arr[i]){
+                result[i] = arr[i*i];
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         // 배열 뒤집기
-        int[] result = reverseArr(new int[]{1, 2, 3, 4, 5});
+        int[] result = Q1ReverseArr(new int[]{1, 2, 3, 4, 5});
         for (int i : result) {
             System.out.println("i = " + i);
         }
